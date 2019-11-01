ab12864bd0814826114e1e9a618e906606071eff
Jake Bernstein <jgbernstein@gmail.com>
2019-10-31 20:11:45 -0400

    Got InterruptTest.ino working

--- InterruptTest/InterruptTest.ino
+++ InterruptTest/InterruptTest.ino
@@ -39,20 +39,32 @@ void setup() {
   Serial1.setTX(26);
   Serial1.setRX(27);
   Serial1.begin(SERIALBAUD,SERIAL_8N1);
-  Serial1.println("Interrupt test");  
+  Serial.begin(115200);
+  Serial1.println("Interrupt test doot doot");  
+  //Serial.println("Interrupt test");
+  if (tempsensor.begin() == true) {
+    Serial1.println("Temperature Sensor ready to go");
+  }
+  SPI1.setMOSI(_MOSI);
+  SPI1.setMISO(_MISO);
+  SPI1.setSCK(_SCK);
+  SPI1.begin();
   attachInterrupt(digitalPinToInterrupt(INTERRUPTPIN), SimulateRead, FALLING);
 }
 
 void loop() {
   uint32_t timenow = millis();
   char outline[100];
+  //sprintf(outline,"Time: %d, nextread: %d, nextbeat: %d",timenow,nextread, nextbeat);
+  //Serial1.println(outline);
   if (timenow > nextread) {
+    //Serial1.println("Read IDs");
     checkI2C();
     checkSPI();
     nextread += SENSORREADPERIODMILLIS;
   }
   if (timenow > nextbeat) {
-    //sprintf(outline);
+    sprintf(outline,"Heartbeat: %d I2C reads, %d I2C faults, %d SPI reads, %d SPI faults, %d Interrupts",numI2Creads, numI2Cfails, numSPIreads, numSPIfails, numFPGAreads);
     Serial1.println(outline);
     nextbeat += HEARTBEATPERIODMILLIS;
   }
@@ -66,19 +78,23 @@ void SimulateRead() {
 
 void checkI2C() {
   uint16_t id;
+  //Serial1.println("Start I2C check");
   id = tempsensor.readRegister(TEMP_ID_REGISTER);
   if (id != TEMP_ID_VALUE) {
     numI2Cfails++;
     Serial1.println("I2C Register Read Failed");
   }  
   numI2Creads++;
+  //Serial1.println("I2C check finished");
 }
 
 void checkSPI() {
+  //Serial1.println("Start SPI check");
   uint16_t id = tiltsensor.readRegister(TILT_ID_REGISTER);
   if (id != TILT_ID_VALUE) {
     numSPIfails++;
     Serial1.println("SPI Register Read Failed");
   }
   numSPIreads++;
+  //Serial1.println("SPI check finished");
 }
