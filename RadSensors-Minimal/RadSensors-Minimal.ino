5d08344fc0b813c391e5197457aaaca097b7b274
Jake Bernstein <jgbernstein@gmail.com>
2020-03-08 20:18:26 -0400

    Updated RadSensors-Minimal.ino to use buzzer

--- RadSensors-Minimal/RadSensors-Minimal.ino
+++ RadSensors-Minimal/RadSensors-Minimal.ino
@@ -2,7 +2,7 @@
 #include "FOL-TMP117.h"
 #include "FOL-ADIS16209.h"
 
-#define SERIALOUT Serial
+#define SERIALOUT Serial1
 
 
 
@@ -11,12 +11,21 @@
 #define _SCK 32
 #define _MISO 0
 #define _MOSI 1
+#define _BUZZ 37
+
+const int MEASTEMP = 1;
+const int MEASTILT = 0;
 
 TMP117 tempsensor; // Initalize sensor
-ADIS16209 tiltsensor; // Initialize tilt sensor
+uint16_t rawTemp;
+float TempC;
 
-uint16_t rawTemp, rawTilt;
-float TempC, angle;
+ADIS16209 tiltsensor; // Initialize tilt sensor
+uint16_t rawTilt;
+float angle;
+
+
+int buzzstate = LOW;
 
 void setup() {
   // Initialize Serial
@@ -26,59 +35,90 @@ void setup() {
   Serial1.begin(115200,SERIAL_8N1);    // Start serial communication at 115200 baud
 
   // Initialize TMP117 I2C Temperature Sensor
-  if (tempsensor.setupSensor() == true) {
-    SERIALOUT.println("TMP117 set up successfully");
-  }
-  else {
-    SERIALOUT.println("TMP117 failed to set up");
+  if (MEASTEMP) {
+    if (tempsensor.setupSensor() == true) {
+      SERIALOUT.println("TMP117 set up successfully");
+    }
+    else {
+      SERIALOUT.println("TMP117 failed to set up");
+    }
   }
 
   // Initialize ADIS16209 Tilt Sensor
-  if (tiltsensor.setupSensor() == true) {
-    SERIALOUT.println("ADIS16209 set up successfully");
+  if (MEASTILT) {
+    if (tiltsensor.setupSensor() == true) {
+      SERIALOUT.println("ADIS16209 set up successfully");
+    }
+    else{
+      SERIALOUT.println("ADIS16209 failed to set up");
+    }
+    tiltsensor.transceiveSensor(PROD_ID);
+    delayMicroseconds(40);
   }
-  else{
-    SERIALOUT.println("ADIS16209 failed to set up");
-  }
-  tiltsensor.transceiveSensor(PROD_ID);
-  delayMicroseconds(40);
+
+pinMode(_BUZZ, OUTPUT);
+
+SERIALOUT.println("Press space to toggle buzzer on/off");
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //temp sensor read
-  rawTemp = tempsensor.readSensor();
-  TempC = rawTemp * TMP117_RESOLUTION;
-  SERIALOUT.print("Temperature in C: ");
-  SERIALOUT.println(TempC);
-  
-  //tilt sensor read
-  rawTilt = tiltsensor.transceiveSensor(XINCL_OUT);
-  SERIALOUT.print("Product ID: ");
-  //SERIALOUT.println(angle);
-  SERIALOUT.println(rawTilt);
-  delayMicroseconds(40);
+  if (MEASTEMP)
+  {
+    rawTemp = tempsensor.readSensor();
+    TempC = rawTemp * TMP117_RESOLUTION;
+    SERIALOUT.print("Temperature in C: ");
+    SERIALOUT.println(TempC);
+  }
 
-  rawTilt = tiltsensor.transceiveSensor(YINCL_OUT);
-  SERIALOUT.print("X inclination in degrees: ");
-  angle = rawTilt * SCALE_ANGLE;
-  //SERIALOUT.println(angle);
-  SERIALOUT.println(rawTilt);
-  delayMicroseconds(40);
+  if (MEASTILT) {
+    //tilt sensor read
+    rawTilt = tiltsensor.transceiveSensor(XINCL_OUT);
+    SERIALOUT.print("Product ID: ");
+    //SERIALOUT.println(angle);
+    SERIALOUT.println(rawTilt);
+    delayMicroseconds(40);
 
-  rawTilt = tiltsensor.transceiveSensor(ROT_OUT);
-  SERIALOUT.print("Y inclination in degrees: ");
-  angle = rawTilt * SCALE_ANGLE;
-  //SERIALOUT.println(angle);
-  SERIALOUT.println(rawTilt);
-  delayMicroseconds(40);
+    rawTilt = tiltsensor.transceiveSensor(YINCL_OUT);
+    SERIALOUT.print("X inclination in degrees: ");
+    angle = rawTilt * SCALE_ANGLE;
+    //SERIALOUT.println(angle);
+    SERIALOUT.println(rawTilt);
+    delayMicroseconds(40);
 
-  rawTilt = tiltsensor.transceiveSensor(PROD_ID);
-  SERIALOUT.print("Rotation in degrees: ");
-  angle = rawTilt * SCALE_ANGLE;
-  //SERIALOUT.println(angle);
-  SERIALOUT.println(rawTilt);
-  delayMicroseconds(40);
+    rawTilt = tiltsensor.transceiveSensor(ROT_OUT);
+    SERIALOUT.print("Y inclination in degrees: ");
+    angle = rawTilt * SCALE_ANGLE;
+    //SERIALOUT.println(angle);
+    SERIALOUT.println(rawTilt);
+    delayMicroseconds(40);
+
+    rawTilt = tiltsensor.transceiveSensor(PROD_ID);
+    SERIALOUT.print("Rotation in degrees: ");
+    angle = rawTilt * SCALE_ANGLE;
+    //SERIALOUT.println(angle);
+    SERIALOUT.println(rawTilt);
+    delayMicroseconds(40);
+  }
+
+//Check for command to toggle buzzer
+if (SERIALOUT.available() > 0) {
+  int inbyte = SERIALOUT.read();
+  if (inbyte == ' ') {
+    SERIALOUT.print("Toggling buzzer ");
+    if (buzzstate == LOW) {
+      buzzstate = HIGH;
+      SERIALOUT.println("HIGH");
+    }
+    else {
+      buzzstate = LOW;
+      SERIALOUT.println("LOW");
+    }
+    digitalWrite(_BUZZ, buzzstate);
+  }
+}
 
   delay(1000);
 }
