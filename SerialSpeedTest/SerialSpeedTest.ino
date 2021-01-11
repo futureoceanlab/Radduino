cc7a895fcc87677925bdcb307d56e8871b8625fa
Jake Bernstein <jgbernstein@gmail.com>
2021-01-11 16:35:17 -0500

    Added SerialSpeedTest.ino

---
+++ SerialSpeedTest/SerialSpeedTest.ino
@@ -0,0 +1,65 @@
+/*
+  SerialPassthrough sketch
+
+  Some boards, like the Arduino 101, the MKR1000, Zero, or the Micro, have one
+  hardware serial port attached to Digital pins 0-1, and a separate USB serial
+  port attached to the IDE Serial Monitor. This means that the "serial
+  passthrough" which is possible with the Arduino UNO (commonly used to interact
+  with devices/shields that require configuration via serial AT commands) will
+  not work by default.
+
+  This sketch allows you to emulate the serial passthrough behaviour. Any text
+  you type in the IDE Serial monitor will be written out to the serial port on
+  Digital pins 0 and 1, and vice-versa.
+
+  On the 101, MKR1000, Zero, and Micro, "Serial" refers to the USB Serial port
+  attached to the Serial Monitor, and "Serial1" refers to the hardware serial
+  port attached to pins 0 and 1. This sketch will emulate Serial passthrough
+  using those two Serial ports on the boards mentioned above, but you can change
+  these names to connect any two serial ports on a board that has multiple ports.
+
+  created 23 May 2016
+  by Erik Nyquist
+*/
+elapsedMicros speedstart;
+
+void setup() {
+  Serial1.setTX(26);
+  Serial1.setRX(27);
+  Serial.begin(9600);
+  Serial.println("Welcome to the speedtest, hit 'r' to start")
+}
+
+void loop() {
+  static char cmd='0';
+  if (Serial.available()) {      // If anything comes in Serial (USB),
+    cmd = Serial.read()
+    switch (cmd) {
+        case 'r':
+            speedtest();
+            break;
+    }
+  }
+
+
+}
+
+void speedtest(int datarate = 115200, int uinterval = 1000, int nmsgs = 1000) {
+    Serial.println("Starting serial test")
+    Serial1.begin(datarate)
+    static int nsent = 0;
+    static uint16 packet[4] = {0xFC, uinterval, 0, 0};
+    nextsend = uinterval;
+    speedstart = 0;
+    while (nsent < nmsgs) {
+        if (speedstart >= nextsend) {
+            Serial1.write(packet,8);
+            nsent++;
+            nextsend += uinterval;
+            packet[1] = speedstart;
+            packet[3] = nsent;
+        }
+    }
+    Serial1.end()
+    Serial.println("Serial test complete")
+}
